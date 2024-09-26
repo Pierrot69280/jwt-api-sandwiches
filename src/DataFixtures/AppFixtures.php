cf411b7a99c45050c51d5c28aa9c69a10b0edabc
Pierre <pierre.nogaro@gmail.com>
2024-09-26 13:54:48 +0200

    fixtures & faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Sandwich;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager): void
+    public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $ingredients = [
+            'Ham', 'Cheese', 'Lettuce', 'Tomato', 'Cucumber',
+            'Onion', 'Chicken', 'Bacon', 'Avocado', 'Mayonnaise',
+            'Mustard', 'Peppers', 'Bell pepper', 'Egg', 'Tuna',
+            'Barbecue sauce', 'Hot sauce', 'Goat cheese',
+            'Arugula', 'Carrot'
+        ];
+
+
+        // create 20 sandwich
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 20; $i++) {
+            $sandwich = new Sandwich();
+
+            $sandwich->setName($faker->name() . ' Sandwich');
+            $sandwich->setPrice(mt_rand(10, 100));
+            $manager->persist($sandwich);
+        }
 
         $manager->flush();
     }
