c04254179a25950945fbf20de2f64b2d7807b2d6
Pierre <pierre.nogaro@gmail.com>
2024-10-01 12:59:20 +0200

    delete & show

--- src/Controller/SandwichController.php
+++ src/Controller/SandwichController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Sandwich;
 use App\Repository\SandwichRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\Serializer;
@@ -17,4 +20,21 @@ class SandwichController extends AbstractController
 
         return $this->json($sandwiches, 200, [],['groups' => ['sandwichesjson']]);
     }
+
+    #[Route('/api/show/{id}', name: 'app_sandwich_show', methods: ['GET'])]
+    public function show(Sandwich $sandwich): Response
+    {
+
+        return $this->json($sandwich, 200, [], ['groups' => ['sandwichesjson']]);
+    }
+
+    #[Route('/api/delete/{id}', name: 'app_sandwich_delete', methods: ['DELETE'])]
+    public function delete(Sandwich $sandwich, EntityManagerInterface $manager): Response
+    {
+        $manager->remove($sandwich);
+        $manager->flush();
+
+        return $this->json(['message' => 'Sandwich deleted successfully'], 200, [], ['groups' => ['sandwichesjson']]);
+    }
 }
+
