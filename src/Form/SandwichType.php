4b19a4de0453498022e906e75131f020797acf2e
Pierre <pierre.nogaro@gmail.com>
2024-10-01 14:51:25 +0200

    Sandwich Type

---
+++ src/Form/SandwichType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Ingredients;
+use App\Entity\Sandwich;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SandwichType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('ingredients', EntityType::class, [
+                'class' => Ingredients::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sandwich::class,
+        ]);
+    }
+}
